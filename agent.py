8c2bf82b65b71e9461e60144b8565f17c437e0e9
fruitpicker01 <54317175+fruitpicker01@users.noreply.github.com>
2024-08-18 14:43:56 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,7 +2,9 @@ from langchain.chat_models.gigachat import GigaChat
 from langgraph.prebuilt import create_react_agent
 
 # Используем модель GigaChat-Pro с соответствующими ключами
-model = GigaChat(credentials="GIGACHAT_CREDENTIALS", model="GigaChat-Pro", verify_ssl_certs=False)
+model = GigaChat(credentials="ZDRkMGFhNmQtN2NjYS00NDMxLWIxNTAtZTc5NDJhZmM1NThiOjYyODE4MWJmLTM5ZjAtNGI4MC05NWU3LWFhYWY4NjRlYmU0YQ==", 
+                 model="GigaChat-Pro", 
+                 verify_ssl_certs=False)
 
 # Пока оставляем инструменты пустыми
 tools = []
